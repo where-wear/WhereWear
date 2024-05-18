10a930def8cd85b02d445805495ead4908f257b7
zyam1 <zyam8441@gmail.com>
2024-05-18 23:24:38 +0900

    feat:next auth 설치

---
+++ client/app/signin/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import KakaoLogin from '@/components/SocialSignin/KakaoLogin';
+const page = () => {
+  return <KakaoLogin />;
+};
+
+export default page;
---
+++ client/components/SocialSignin/KakaoLogin.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const KakaoLogin = () => {
+  // 클릭시 카카오 로그인
+  return (
+    <>
+      <div>카카오 로그인</div>
+      <button>카카오 로그인 버튼</button>
+      {/* 나중에css가져와야함 */}
+    </>
+  );
+};
+
+export default KakaoLogin;
