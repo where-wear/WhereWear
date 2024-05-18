c998a6ea50e0d42f98c5e39836ae40177f28d3a0
zyam1 <zyam8441@gmail.com>
2024-05-19 01:07:33 +0900

    client:카카오 로그인 쿠키에 토큰저장

--- client/components/SocialSignin/KakaoLogin.tsx
+++ client/components/SocialSignin/KakaoLogin.tsx
@@ -1,11 +1,13 @@
+'use client';
 import React from 'react';
+import { signIn } from 'next-auth/react';
 
 const KakaoLogin = () => {
   // 클릭시 카카오 로그인
   return (
     <>
       <div>카카오 로그인</div>
-      <button>카카오 로그인 버튼</button>
+      <button onClick={() => signIn('kakao')}>카카오 로그인 버튼</button>
       {/* 나중에css가져와야함 */}
     </>
   );
