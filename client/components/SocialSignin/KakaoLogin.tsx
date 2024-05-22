774fd4fcd550f6d6f833c8aa0150b46096af74ef
zyam1 <zyam8441@gmail.com>
2024-05-22 22:56:21 +0900

    client:카카오 로그인 api 요청

--- client/components/SocialSignin/KakaoLogin.tsx
+++ client/components/SocialSignin/KakaoLogin.tsx
@@ -1,24 +1,46 @@
-// 이미지랑 닉네임 가져옴
+// // 이미지랑 닉네임 가져옴
+// 'use client';
+// import React from 'react';
+// import { signIn } from 'next-auth/react';
+// import SignOut from './SignOut';
+// //로그인한 유저 정보 가져오기
+
+// const KakaoLogin = () => {
+//   // 클릭시 카카오 로그인
+//   return (
+//     <>
+//       <div>
+//         <img
+//           style={{ cursor: 'pointer' }}
+//           onClick={() => signIn('kakao', { callbackUrl: '/user' })}
+//           src="/image/kakao_login_medium_wide.png"
+//         />
+//       </div>
+
+//       <SignOut />
+//     </>
+//   );
+// };
+
+// export default KakaoLogin;
+
 'use client';
 import React from 'react';
-import { signIn } from 'next-auth/react';
-import SignOut from './SignOut';
-//로그인한 유저 정보 가져오기
 
 const KakaoLogin = () => {
-  // 클릭시 카카오 로그인
-  return (
-    <>
-      <div>
-        <img
-          style={{ cursor: 'pointer' }}
-          onClick={() => signIn('kakao', { callbackUrl: '/user' })}
-          src="/image/kakao_login_medium_wide.png"
-        />
-      </div>
+  const handleLogin = () => {
+    window.location.href = 'http://localhost:8080/oauth/authorization/kakao';
+  };
 
-      <SignOut />
-    </>
+  return (
+    <div>
+      <img
+        style={{ cursor: 'pointer' }}
+        onClick={handleLogin}
+        src="/image/kakao_login_medium_wide.png"
+        alt="카카오 로그인"
+      />
+    </div>
   );
 };
 
--- client/components/SocialSignin/SignOut.tsx
+++ client/components/SocialSignin/SignOut.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import { signOut } from 'next-auth/react';
 const SignOut = () => {
