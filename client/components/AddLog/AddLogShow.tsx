b2d3f20fd8a4e828baa0d69afb39abdd1e9e59d3
zyam1 <zyam8441@gmail.com>
2024-06-10 00:13:27 +0900

    로그 공개여부 판단 바 만들기

--- client/components/AddLog/AddLogShow.tsx
+++ client/components/AddLog/AddLogShow.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
+//todo: 누르면 구슬이 이동하는 애니메이션 넣기 데이터 값 bool값으로
+//지금은 그냥 백그라운드 색상이 바뀌는것으로
 
 const AddLogShow = () => {
-  return <div>AddLogShow</div>;
+  const [isShow, setIsShow] = useState<boolean>(true);
+
+  // 전체 공개 여부 핸들러
+  const logIsShowHandler = () => {
+    setIsShow((prevIsShow) => !prevIsShow);
+  };
+
+  useEffect(() => {}, [isShow]);
+
+  return (
+    <>
+      <div className="add-log-show-container">
+        <div
+          onClick={logIsShowHandler}
+          className={`add-log-show-background ${
+            isShow ? 'add-log-show-yes' : 'add-log-show-no'
+          }`}
+        >
+          <div className="add-log-circle"></div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AddLogShow;
