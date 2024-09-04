13460c28181bc16f72842808312bfac1a2396876
unknown <zyam8441@gmail.com>
2024-09-04 23:38:06 +0900

    닉네임 중복확인 state생성

--- client/components/SignUpDetail/SignData.tsx
+++ client/components/SignUpDetail/SignData.tsx
@@ -1,16 +1,22 @@
-'use client';
-import React, { useState } from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { UserDataType } from '@/types/type';
-import axios from 'axios';
+"use client";
+import React, { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { UserDataType } from "@/types/type";
+import axios from "axios";
 
 const SignData = () => {
+  //닉네임 체크 관련 변수
+  const [CheckNickname, setCheckNickname] = useState<string>("");
+
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm<UserDataType>();
+  const ninckNameCheckHandler = () => {
+    //닉네임 체크하는 axios요청 함수
+  };
 
   const onSubmitHandler: SubmitHandler<UserDataType> = async (data) => {
     const transformedData = {
@@ -19,16 +25,16 @@ const SignData = () => {
       weight: Number(data.weight),
       footSize: Number(data.footSize),
     };
-    console.log('버튼 입력시 들어가는 데이터', transformedData);
+    console.log("버튼 입력시 들어가는 데이터", transformedData);
     // 회원 정보 입력 api 요청
     try {
       const response = await axios.post(
-        'http://localhost:8080/api/accounts/signUp',
+        "http://localhost:8080/api/accounts/signUp",
         transformedData
       );
-      console.log('서버 응답:', response.data);
+      console.log("서버 응답:", response.data);
     } catch (error) {
-      console.error('서버 요청 중 오류 발생:', error);
+      console.error("서버 요청 중 오류 발생:", error);
     }
   };
   // 나중에 주소 env 처리 해줘야함
@@ -37,37 +43,38 @@ const SignData = () => {
     <>
       <form onSubmit={handleSubmit(onSubmitHandler)}>
         <label>계정명</label>
-        <input {...register('nickname', { required: true, maxLength: 20 })} />
-        {errors.nickname && errors.nickname.type === 'required' && (
+        <input {...register("nickname", { required: true, maxLength: 20 })} />
+        <button onClick={ninckNameCheckHandler}>중복 확인</button>
+        {errors.nickname && errors.nickname.type === "required" && (
           <div>닉네임을 입력해 주세요!</div>
           // 나중에 붉은색으로 css입혀야함
         )}
         <label>키</label>
-        <input {...register('height', { required: true, maxLength: 300 })} />
-        {errors.height && errors.height.type === 'required' && (
+        <input {...register("height", { required: true, maxLength: 300 })} />
+        {errors.height && errors.height.type === "required" && (
           <div>키를 입력해 주세요!</div>
           // 나중에 붉은색으로 css입혀야함
         )}
         <label>몸무게</label>
-        <input {...register('weight', { required: true, maxLength: 1000 })} />
-        {errors.weight && errors.weight.type === 'required' && (
+        <input {...register("weight", { required: true, maxLength: 1000 })} />
+        {errors.weight && errors.weight.type === "required" && (
           <div>몸무게를 입력해 주세요!</div>
           // 나중에 붉은색으로 css입혀야함
         )}
         <label>발사이즈</label>
-        <input {...register('footSize', { required: true, maxLength: 500 })} />
-        {errors.footSize && errors.footSize.type === 'required' && (
+        <input {...register("footSize", { required: true, maxLength: 500 })} />
+        {errors.footSize && errors.footSize.type === "required" && (
           <div>발사이즈를 입력해 주세요!</div>
           // 나중에 붉은색으로 css입혀야함
         )}
         <label>직업</label>
-        <input {...register('job', { required: true, maxLength: 20 })} />
-        {errors.job && errors.job.type === 'required' && (
+        <input {...register("job", { required: true, maxLength: 20 })} />
+        {errors.job && errors.job.type === "required" && (
           <div>직업을 입력해 주세요!</div>
           // 나중에 붉은색으로 css입혀야함
         )}
         <label>소개글</label>
-        <input {...register('introduction')} />
+        <input {...register("introduction")} />
         <button type="submit">등록</button>
       </form>
     </>
