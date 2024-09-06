6543d92f4167e4c100a7a14fbefe7ea78b85e394
unknown <zyam8441@gmail.com>
2024-09-06 13:48:49 +0900

    계정명 중복확인 요청

--- client/components/SignUpDetail/SignData.tsx
+++ client/components/SignUpDetail/SignData.tsx
@@ -1,9 +1,10 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { UserDataType } from "@/types/type";
 import axios from "axios";
-
+import BackBar from "../Global/BackBar";
+import Cookies from "js-cookie";
 const SignData = () => {
   //닉네임 체크 관련 변수
   const [CheckNickname, setCheckNickname] = useState<string>("");
@@ -14,8 +15,22 @@ const SignData = () => {
     watch,
     formState: { errors },
   } = useForm<UserDataType>();
-  const ninckNameCheckHandler = () => {
-    //닉네임 체크하는 axios요청 함수
+  const ninckNameCheckHandler = async () => {
+    //닉네임 체크하는 axios요청 함수 checkNickname 변수를 담아서 보내기
+    try {
+      const refreshToken = Cookies.get("refresh_token");
+      const response = await axios.post(
+        "http://localhost:8080/api/accounts/nicknameCheck",
+        {
+          params: { nickname: CheckNickname },
+
+          headers: {
+            Authorization: `Bearer ${refreshToken}`, // 헤더에 토큰 추가
+          },
+        }
+      );
+      console.log(response);
+    } catch (error) {}
   };
 
   const onSubmitHandler: SubmitHandler<UserDataType> = async (data) => {
@@ -41,41 +56,71 @@ const SignData = () => {
   // header 에 토큰 값 넣어야함
   return (
     <>
+      <BackBar text=" 추가 정보 입력 " />
+
       <form onSubmit={handleSubmit(onSubmitHandler)}>
-        <label>계정명</label>
-        <input {...register("nickname", { required: true, maxLength: 20 })} />
-        <button onClick={ninckNameCheckHandler}>중복 확인</button>
+        <div>
+          <label>계정명</label>
+          <input
+            {...register("nickname", { required: true, maxLength: 20 })}
+            value={CheckNickname}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setCheckNickname(e.target.value);
+            }}
+          />
+          <button onClick={ninckNameCheckHandler}>중복 확인</button>
+        </div>
+
         {errors.nickname && errors.nickname.type === "required" && (
           <div>닉네임을 입력해 주세요!</div>
           // 나중에 붉은색으로 css입혀야함
         )}
-        <label>키</label>
-        <input {...register("height", { required: true, maxLength: 300 })} />
+        <div>
+          <label>키</label>
+          <input {...register("height", { required: true, maxLength: 300 })} />
+        </div>
         {errors.height && errors.height.type === "required" && (
           <div>키를 입력해 주세요!</div>
           // 나중에 붉은색으로 css입혀야함
         )}
-        <label>몸무게</label>
-        <input {...register("weight", { required: true, maxLength: 1000 })} />
+        <div>
+          <label>몸무게</label>
+          <input {...register("weight", { required: true, maxLength: 1000 })} />
+        </div>
+
         {errors.weight && errors.weight.type === "required" && (
           <div>몸무게를 입력해 주세요!</div>
           // 나중에 붉은색으로 css입혀야함
         )}
-        <label>발사이즈</label>
-        <input {...register("footSize", { required: true, maxLength: 500 })} />
+        <div>
+          <label>발사이즈</label>
+          <input
+            {...register("footSize", { required: true, maxLength: 500 })}
+          />
+        </div>
+
         {errors.footSize && errors.footSize.type === "required" && (
           <div>발사이즈를 입력해 주세요!</div>
           // 나중에 붉은색으로 css입혀야함
         )}
-        <label>직업</label>
-        <input {...register("job", { required: true, maxLength: 20 })} />
+        <div>
+          <label>직업</label>
+          <input {...register("job", { required: true, maxLength: 20 })} />
+        </div>
+
         {errors.job && errors.job.type === "required" && (
           <div>직업을 입력해 주세요!</div>
           // 나중에 붉은색으로 css입혀야함
         )}
-        <label>소개글</label>
-        <input {...register("introduction")} />
-        <button type="submit">등록</button>
+        <div>
+          <label>소개글</label>
+          <input {...register("introduction")} />
+        </div>
+
+        <label className="user-data-sub"></label>
+        <button type="submit" className="user-data-sub">
+          입력 완료
+        </button>
       </form>
     </>
   );
--- client/components/SocialSignin/KakaoLogin.tsx
+++ client/components/SocialSignin/KakaoLogin.tsx
@@ -1,42 +1,15 @@
-// // 이미지랑 닉네임 가져옴
-// 'use client';
-// import React from 'react';
-// import { signIn } from 'next-auth/react';
-// import SignOut from './SignOut';
-// //로그인한 유저 정보 가져오기
-
-// const KakaoLogin = () => {
-//   // 클릭시 카카오 로그인
-//   return (
-//     <>
-//       <div>
-//         <img
-//           style={{ cursor: 'pointer' }}
-//           onClick={() => signIn('kakao', { callbackUrl: '/user' })}
-//           src="/image/kakao_login_medium_wide.png"
-//         />
-//       </div>
-
-//       <SignOut />
-//     </>
-//   );
-// };
-
-// export default KakaoLogin;
-//위는 next auth 사용했을경우
-
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
 
 const KakaoLogin = () => {
   const handleLogin = () => {
-    window.location.href = 'http://localhost:8080/oauth2/authorization/kakao';
+    window.location.href = "http://localhost:8080/oauth2/authorization/kakao";
   };
 
   return (
     <div>
       <img
-        style={{ cursor: 'pointer' }}
+        style={{ cursor: "pointer" }}
         onClick={handleLogin}
         src="/image/kakao_login_medium_wide.png"
         alt="카카오 로그인"
