c0a6adc138ca48814dc447974354b4db22fa4cd1
zyam1 <zyam8441@gmail.com>
2024-10-08 23:47:43 +0900

    env 수정

--- client/app/Log/[logId]/page.tsx
+++ client/app/Log/[logId]/page.tsx
@@ -1,24 +1,24 @@
 //로그 보여지는 화면-> url 로그번호
-"use client";
-import React, { useEffect } from "react";
-import { useParams } from "next/navigation";
-import axios from "axios";
-import BackBar from "@/components/Global/BackBar";
+'use client';
+import React, { useEffect } from 'react';
+import { useParams } from 'next/navigation';
+import axios from 'axios';
+import BackBar from '@/components/Global/BackBar';
 const page = () => {
   //태그 나중에 flex-wrap해줘야함
-  const testTag: string[] = ["태그예시1", "태그예시2", "태그긴문장예시3"];
+  const testTag: string[] = ['태그예시1', '태그예시2', '태그긴문장예시3'];
   const testImgUrl: string[] = [
-    "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDEwfHxmbG93ZXJ8ZW58MHx8fHwxNjEyNzI4MTU0&ixlib=rb-1.2.1&q=80&w=400",
-    "https://images.unsplash.com/photo-1542273917363-3b1817f69a2d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGJvYXQlMjBvY2VhbnxlbnwwfHx8fDE2MDA5ODUyODg&ixlib=rb-1.2.1&q=80&w=400",
-    "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDE3fHxkZXNlcnQlMjBsYW5kc2NhcGV8ZW58MHx8fHwxNjEyNzMwMDEw&ixlib=rb-1.2.1&q=80&w=400",
-    "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDE3fHxkZXNlcnQlMjBsYW5kc2NhcGV8ZW58MHx8fHwxNjEyNzMwMDEw&ixlib=rb-1.2.1&q=80&w=400",
-    "https://images.unsplash.com/photo-1517511620798-cec17d428bc0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGZvb2R8ZW58MHx8fHwxNjEyNzMwMTQy&ixlib=rb-1.2.1&q=80&w=400",
-    "https://images.unsplash.com/photo-1521747116042-5a810fda9664?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDF8fHRyZWV8ZW58MHx8fHwxNjEyNzMwMzE1&ixlib=rb-1.2.1&q=80&w=400",
+    'https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDEwfHxmbG93ZXJ8ZW58MHx8fHwxNjEyNzI4MTU0&ixlib=rb-1.2.1&q=80&w=400',
+    'https://images.unsplash.com/photo-1542273917363-3b1817f69a2d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGJvYXQlMjBvY2VhbnxlbnwwfHx8fDE2MDA5ODUyODg&ixlib=rb-1.2.1&q=80&w=400',
+    'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDE3fHxkZXNlcnQlMjBsYW5kc2NhcGV8ZW58MHx8fHwxNjEyNzMwMDEw&ixlib=rb-1.2.1&q=80&w=400',
+    'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDE3fHxkZXNlcnQlMjBsYW5kc2NhcGV8ZW58MHx8fHwxNjEyNzMwMDEw&ixlib=rb-1.2.1&q=80&w=400',
+    'https://images.unsplash.com/photo-1517511620798-cec17d428bc0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGZvb2R8ZW58MHx8fHwxNjEyNzMwMTQy&ixlib=rb-1.2.1&q=80&w=400',
+    'https://images.unsplash.com/photo-1521747116042-5a810fda9664?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDF8fHRyZWV8ZW58MHx8fHwxNjEyNzMwMzE1&ixlib=rb-1.2.1&q=80&w=400',
   ];
   const testImgUrl2: string[] = [
-    "https://via.placeholder.com/300x200.png?text=80x80",
-    "https://via.placeholder.com/300x200.png?text=80x80",
-    "https://via.placeholder.com/300x200.png?text=80x80",
+    'https://via.placeholder.com/300x200.png?text=80x80',
+    'https://via.placeholder.com/300x200.png?text=80x80',
+    'https://via.placeholder.com/300x200.png?text=80x80',
   ];
 
   const { logId } = useParams();
@@ -33,7 +33,7 @@ const page = () => {
   const getLogData = async () => {
     try {
       const response = await axios.get(
-        `${process.env.NEXT_PUBLIC_API_URL}/log/getLog`,
+        `${process.env.NEXT_PUBLIC_API_URL}/api/log/getLog`,
         {
           params: { id: logId },
         }
@@ -99,10 +99,10 @@ const page = () => {
       <div className="log-item-container">
         <div className="log-item-title">패션정보</div>
         <div className="log-item-inner">
-          상의 {">"} 니트/스웨터 {">"} {"'"}폴로 반팔 니트{"'"}
+          상의 {'>'} 니트/스웨터 {'>'} {"'"}폴로 반팔 니트{"'"}
         </div>
         <div className="log-item-inner">
-          상의 {">"} 니트/스웨터 {">"} {"'"}폴로 반팔 니트{"'"}
+          상의 {'>'} 니트/스웨터 {'>'} {"'"}폴로 반팔 니트{"'"}
         </div>
       </div>
       <hr className="log-hr" />
