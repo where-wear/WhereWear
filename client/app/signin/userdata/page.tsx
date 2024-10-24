9e5ca147a20a2692823ab9afb034412880cf07a0
zyam1 <zyam8441@gmail.com>
2024-10-24 13:08:23 +0900

    배포환경으로 셋팅

--- client/app/signin/userdata/page.tsx
+++ client/app/signin/userdata/page.tsx
@@ -3,11 +3,13 @@
 'use client';
 import SignData from '@/components/SignUpDetail/SignData';
 import React from 'react';
-
+import { Suspense } from 'react';
 const page = () => {
   return (
     <>
-      <SignData />
+      <Suspense>
+        <SignData />
+      </Suspense>
     </>
   );
 };
