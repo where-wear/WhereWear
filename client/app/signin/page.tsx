33d6798f129a3e11563f4b820c44c57636dbcaed
zyam1 <zyam8441@gmail.com>
2024-11-20 21:34:11 +0900

    소셜로그인 화면 수정, 둘러볼게요 누르면 토큰값 날리기

--- client/app/signin/page.tsx
+++ client/app/signin/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import KakaoLogin from '@/components/SocialSignin/KakaoLogin';
 import Link from 'next/link';
@@ -5,11 +6,20 @@ import NaverLogin from '@/components/SocialSignin/NaverLogin';
 import GoogleLogin from '@/components/SocialSignin/GoogleLogin';
 
 const page = () => {
+  const deleteToken = () => {
+    localStorage.removeItem('accessToken');
+  };
   return (
     <>
       <div className="home-container">
-        <img src="/image/home_main_text.png" className="home-main-text" />
-        <img src="/image/home_sub_text.png" className="home-sub-text" />
+        <div className="home-container">
+          <div
+            style={{ whiteSpace: 'pre-line' }}
+            className="home-main-text"
+          >{`어디에 \n 뭐 입고 가지?`}</div>
+
+          <div className="home-sub-text">WHERE WEAR | 웨어웨어</div>
+        </div>
         <div className="snsLogin-container">
           <div className="kakao-login-icon">
             <KakaoLogin />
@@ -21,7 +31,11 @@ const page = () => {
             <GoogleLogin />
           </div>
 
-          <Link href={'/home'} style={{ textDecoration: 'none' }}>
+          <Link
+            href={'/home'}
+            style={{ textDecoration: 'none' }}
+            onClick={deleteToken}
+          >
             <div className="justshow-button">그냥 둘러볼게요!</div>
           </Link>
         </div>
